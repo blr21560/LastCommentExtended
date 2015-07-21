a0cf953f0678667c0a494e3d58486e8fc0339ce9
brol <brol31@gmail.com>
2015-07-21 11:30:06 +0200

    0.3.1

--- _define.php
+++ _define.php
@@ -20,13 +20,13 @@ $this->registerModule(
 	/* Name */			'LastCommentExtended',
 	/* Description*/	'Extensive list of latest comments posted',
 	/* Author */		'Pierre Van Glabeke, Bernard Le Roux',
-	/* Version */		'0.3',
+	/* Version */		'0.3.1',
 	/* Properties */
 	array(
 		'permissions' => 'admin',
 		'type' => 'plugin',
 		'dc_min' => '2.7',
 		'support' => 'http://forum.dotclear.org/viewtopic.php?pid=326232#p326232',
-		'details' => 'https://github.com/blr21560/LastCommentExtended/wiki'
+		'details' => 'https://github.com/brol/LastCommentExtended/wiki'
 	)
 );
