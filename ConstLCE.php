cb0bbf2dd509adf93d7e11c7c60601f5a41dd5b3
Pierre <brol31@gmail.com>
2023-01-11 16:21:38 +0100

    licence, localisation

--- ConstLCE.php
+++ ConstLCE.php
@@ -1,17 +1,14 @@
 <?php
-/*
-# This is 'LastCommentExtended', a plugin for Dotclear 2
-# Copyright (c) 2013-2015
-# Pierre Van Glabeke, Bernard Le Roux.
-#
-# This is an open source software, distributed under the GNU
-# General Public License (version 2) terms and  conditions.
-#
-# You should have received a copy of the GNU General Public
-# License along with 'LCE' (see LICENCE.txt);
-# if not, write to the Free Software Foundation, Inc.,
-# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
+/**
+ * @brief LastCommentExtended, a plugin for Dotclear 2
+ *
+ * @package Dotclear
+ * @subpackage Plugin
+ *
+ * @Pierre Van Glabeke, Bernard Le Roux and contributors
+ *
+ * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
+ */
 if (!defined('DC_RC_PATH')) { 
 	return; 
 }
---
+++ locales/fr/main.lang.php
@@ -0,0 +1,20 @@
+<?php
+// Language: Français
+// Module: LastCommentExtended - 
+// Date: 2023-01-11 15:20:32
+// Author: Pierre Van Glabeke
+// Translated with dcTranslater - 2022.12.26
+
+
+#
+# DOT NOT MODIFY THIS FILE !
+#
+
+l10n::$locales['LastCommentExtended: last comments'] = 'LastCommentExtended : derniers commentaires';
+l10n::$locales['Extensive list of latest comments posted'] = 'Liste étentue des derniers commentaires publiés';
+l10n::$locales['Comments limit:'] = 'Nombre de commentaires maximum :';
+l10n::$locales['Title lenght limit:'] = 'Longueur maximale du titre :';
+l10n::$locales['Comment lenght limit:'] = 'Longueur maximale du commentaire :';
+l10n::$locales['Date format (leave empty to use default blog format):'] = 'Format de la date (laissez vide pour utiliser le format par défaut du blog) :';
+l10n::$locales['String format (%1$s = date; %2$s = title; %3$s = author; %4$s = content of the comment; %5$s = comment URL):'] = 'Format d\'affichage (%1$s = date ; %2$s = titre ; %3$s = auteur ; %4$s = contenu du commentaire ; %5$s = URL du commentaire) :';
+l10n::$locales['Last comments with LCE plugin'] = 'Derniers commentaires par le plugin LCE';
